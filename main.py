0a38e92567c84bf993982c6fc0b7f546a4f245ba
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-12-02 16:17:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from pygame.locals import *
 pygame.init()
 screenInfo = pygame.display.Info()
 size = (width, height) = (screenInfo.current_w, screenInfo.current_h)
+screen = pygame.display.set_mode(size)
