de211862e68e8132b5ee7d2d5fac3e6eacc891b1
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-12-02 16:14:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.locals import * #imports all from pygame
+from pygame.locals import *
 pygame.init()
 screenInfo = pygame.display.Info()
 size = (width, height) = (screenInfo.current_w, screenInfo.current_h)
